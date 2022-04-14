e13a07c4d2aead210f7c352d34ff1bafca68d5ba
JB <julien.bassin@avanade.com>
2022-04-14 06:38:03 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AzLearnGitLab.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
